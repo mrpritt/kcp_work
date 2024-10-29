2a60759d6f64440fc67bb148fea37c7e4de14d8e
Gustavo Lacroix <gustavolacroix@gmail.com>
2024-10-29 17:08:15 -0300

    Experiments with KPC, KP, LP, WMIS

--- src/model.hpp
+++ src/model.hpp
@@ -11,13 +11,38 @@
 using namespace std;
 
 class Model {
-private:
+protected:
     std::unique_ptr<IloEnv> env_;
     std::unique_ptr<IloModel> model_;
     IloNumVarArray x_;
+    IloCplex solver_;
 
 public:
+    virtual void build(const KnapsackData& data)=0;
+    std::pair<std::vector<float>, IloAlgorithm::Status> solve();
+    ~Model();
+};
+
+class KPModel: public Model {
+public:
     void build(const KnapsackData& data);
-    std::pair<std::vector<bool>, IloAlgorithm::Status> solve();
-    std::vector<bool> getSolution(IloCplex solver);
+    pair<std::vector<bool>, float> getSolution(vector<float> solution);
+};
+
+class KPCModel: public Model {
+public:
+    void build(const KnapsackData& data);
+    pair<std::vector<bool>, float> getSolution(vector<float> solution);
+};
+
+class LPModel: public Model {
+public:
+    void build(const KnapsackData& data);
+    pair<std::vector<float>, float> getSolution(vector<float> solution);
+};
+
+class WMISModel: public Model {
+public:
+    void build(const KnapsackData& data);
+    pair<std::vector<bool>, float> getSolution(vector<float> solution);
 };
