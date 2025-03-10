4c409e3bb5b163c0e853868871c2ce056791edd3
Gustavo Lacroix <gustavolacroix@gmail.com>
2025-03-10 09:52:23 -0300

    Source this script to shorten paths to execs

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Abbreviate commands
+kpc() {
+	$HOME/kcp_work/build/build/bin/cplex_kpc "$@"
+}
+
+mwis() {
+	$HOME/kcp_work/build/build/bin/cplex_mwis "$@"
+}
+
+hl() {
+	$HOME/kcp_work/build/build/bin/hl "$@"
+}
