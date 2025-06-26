249f952fb822c0ac2f3574a9f0ad9d3651d9828b
gustavo lacroix <gustavolacroix@gmail.com>
2025-06-26 12:41:51 -0300

    wip

--- src/bb.cpp
+++ src/bb.cpp
@@ -1,37 +1,39 @@
+#include "bbtypes.h"
 #include "kpc.hpp"
 #include "utils.hpp"
 #include <algorithm>
 #include <numeric>
+#include <stack>
 #include <string>
 
 #include "bitscan.h"
 
+/* NOTE:
+ * bitarray.set_bit() sets all bits to 1, including the unused bits of the word
+ */
+
 using namespace std;
 
-using AdjMatrix = vector<vector<bool>>;
 using Partition = vector<vector<int>>;
 using weight_t = int;
 using profit_t = int;
-using Items = vector<int>;
 using Profits = vector<profit_t>;
 using Weights = vector<weight_t>;
 
 typedef struct {
+  int n;
   Profits p;
   Weights w;
   weight_t W;
-  AdjMatrix adj;
+  vector<bitarray> N;
 } KPData;
 
-Items I_inc;
-profit_t LB = 0;
-
-// typedef struct {
-//   bitset I;
-//   profit_t p;
-//   weight_t w;
-//   bitset C;
-// } Node;
+typedef struct {
+  bitarray I;
+  profit_t p;
+  weight_t w;
+  bitarray C;
+} Node;
 
 // PARTITION(n)
 // should_stop = true     # should stop if no items in CC fits in P
@@ -42,7 +44,7 @@ profit_t LB = 0;
 // for i in CC {
 //    if (n.w + w[i] <= c) {
 //        pi_s = max(Pi_s, p[i])
-//        if (C & N[i] == C && 
+//        if (C & N[i] == C &&
 //            LB - n.p >= UB_p + pi_s) { # if i forms clique and budget okay
 //            should_stop = false;
 //            Pi_s = pi_s;
@@ -61,7 +63,7 @@ profit_t LB = 0;
 //    should_stop = true;
 //    for i in CC {
 //        pi_s = max(Pi_s, p[i])
-//        if (C & N[i] == C && 
+//        if (C & N[i] == C &&
 //            LB - n.p >= UB_p + pi_s) { # if i forms clique and budget okay
 //            should_stop = false;
 //            Pi_s = pi_s;
@@ -71,7 +73,56 @@ profit_t LB = 0;
 //    }
 // }
 // # Now CC is the branching set
-//
+bitarray partition(const Node &n, const KPData &data) {
+  bool should_stop = false;
+  bitarray CC(n.C);
+  profit_t UB_p = 0;
+  profit_t Pi_s = 0;
+  bitarray C(n.C); // TODO: refactor
+  C.erase_bit();
+
+  CC.init_scan(bbo::NON_DESTRUCTIVE);
+  int i = CC.next_bit();
+  while (i != EMPTY_ELEM) {
+
+    i = CC.next_bit();
+  }
+
+  while (!should_stop && !CC.is_empty()) {
+    UB_p += Pi_s;
+    Pi_s = 0;
+    C.erase_bit();
+    should_stop = true;
+
+    CC.init_scan(bbo::NON_DESTRUCTIVE);
+    int i = CC.next_bit();
+    while (i != EMPTY_ELEM) {
+      int pi_s = max(Pi_s, data.p[i]);
+
+      i = CC.next_bit();
+    }
+  }
+
+  return CC;
+}
+
+bool should_cut(const Node &n) { return false; }
+
+void print_node(const Node &n) {
+  cout << "Node:\n";
+  n.I.print(cout, false);
+  cout << "\n";
+  cout << n.p << " " << n.w << "\n";
+  n.C.print(cout, false);
+  cout << "\n";
+}
+
+Node add_item(const Node &n, int bit, const KPData &data) {
+  Node new_node = n;
+  new_node.I.set_bit(bit);
+  new_node.C &= data.N[bit];
+  return new_node;
+}
 
 // BRANCH_AND_BOUND(data)
 // # create root node
@@ -86,7 +137,35 @@ profit_t LB = 0;
 //        }
 //    }
 // }
+//
+Node branch_and_bound(const KPData &data, profit_t hLB = 0) {
+  auto ones = bitarray(data.n);
+  ones.set_bit();
+  Node I_inc = {bitarray(data.n), 0, 0, ones};
+  profit_t LB = hLB;
 
+  stack<Node> Q;
+  Q.push(I_inc);
+  while (!Q.empty()) {
+    auto n = Q.top();
+    Q.pop();
+    if (n.p > I_inc.p) {
+      LB = n.p;
+      I_inc = n;
+    }
+    if (!should_cut(n)) {
+      bitarray B = partition(n, data);
+      B.init_scan(bbo::DESTRUCTIVE);
+      auto bit = B.next_bit();
+      while (bit != EMPTY_ELEM) {
+        Q.push(add_item(n, bit, data));
+        bit = B.next_bit();
+      }
+    }
+  }
+
+  return I_inc;
+}
 int main(int argc, char **argv) {
 
   string filename = argv[1];
@@ -99,12 +178,12 @@ int main(int argc, char **argv) {
     return data.p[i] * data.w[j] > data.p[j] * data.w[i];
   });
   data = arrange_data(idx, data);
-  AdjMatrix adj(data.n, vector<bool>(data.n, false));
+  vector<bitarray> adj(data.n, bitarray(data.n));
   for (auto p : data.pairs) {
-    adj[p.first - 1][p.second - 1] = true;
-    adj[p.second - 1][p.first - 1] = true;
+    adj[p.first - 1].set_bit(p.second - 1);
+    adj[p.second - 1].set_bit(p.first - 1);
   }
-  KPData i = {data.p, data.w, data.W, adj};
+  KPData i = {data.n, data.p, data.w, data.W, adj};
 
   // (1) Preprocessing
 
@@ -113,9 +192,8 @@ int main(int argc, char **argv) {
   // (3) Bounds table generation
 
   // (4) Branch & Bound
-  vector<int> v_hat(data.n, 0);
-  iota(idx.begin(), idx.end(), 0);
-  // TODO: make compatible with `I` from heuristic solution
+  Node s = branch_and_bound(i);
+  print_node(s);
 
   return 0;
 }
